5cf9924ad3bac43d68f8b257b4338760fab33d42
Jonathan <jony_004@hotmail.com>
2022-01-09 17:30:50 -0500

    first commit

---
+++ models/sockets.js
@@ -0,0 +1,29 @@
+class Sockets {
+    constructor(io){
+        this.io=io;
+        this.socketEvents();
+    }
+    socketEvents(){
+        this.io.on('connection', (socket) => { 
+            console.log(`socket.id`, socket.handshake.query.nombre);
+            this.io.emit('mensaje-bienvenida','Se conecto el usuario '+socket.handshake.query.nombre);
+            socket.on('mensaje-cliente',(datos)=>{
+                console.log(`mensaje-cliente`, datos);
+                this.io.emit('mensaje-server',socket.handshake.query.nombre+': '+datos);
+            });
+            socket.on("disconnect", () => {
+                this.io.emit('mensaje-bienvenida','Se Desconecto el usuario '+socket.handshake.query.nombre);
+              });
+            socket.on("mensaje-escribiendo", () => {
+                this.io.emit('mensaje-escribiendo','Escribiendo el usuario '+socket.handshake.query.nombre);
+            });  
+
+            socket.on("mensaje-soltando", () => {
+                this.io.emit('mensaje-soltando','');
+            });
+         });
+
+    }
+}
+
+module.exports=Sockets;
